e795b8e095ec0668f4c62ecc16408c7f0c2f0f9e
Ian Cliffe <ian@Ians-MacBook-Pro.local>
2013-08-07 22:18:09 +0100

    Initial Commit

---
+++ AirplayTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AirplayTest
+//
+//  Created by Ian Cliffe on 07/08/2013.
+//  Copyright (c) 2013 Ian Cliffe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AirplayTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AirplayTest
+//
+//  Created by Ian Cliffe on 07/08/2013.
+//  Copyright (c) 2013 Ian Cliffe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
